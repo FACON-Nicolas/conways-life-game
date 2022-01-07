fa3b9d6e2c7c8ee34397ccde9c1a2240f27430df
FACON-Nicolas <84249866+FACON-Nicolas@users.noreply.github.com>
2022-01-07 19:55:39 +0100

    add game functions

--- src/game.py
+++ src/game.py
@@ -10,4 +10,19 @@ class Game:
         self.__platform = GamePlatform.GamePlatform(height, width, 2, 3)
         self.__display = Display(self.__platform.getCaseSize()*self.__platform.getHeight(), 
             self.__platform.getCaseSize()*self.__platform.getWidth()) 
+
+    def run(self):
+        """"""
+        pass
+
+    def event(self):
+        """"""
+        pass
+    
+    def controls(self):
+        """"""
+        pass
+
         
+g = Game()
+g.run()
